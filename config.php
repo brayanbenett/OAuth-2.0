0cb6fe33d0616e3f6321e2d4805e97f85e821d8a
Brayan Benett <38996910+brayanbenett@users.noreply.github.com>
2021-07-15 10:55:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
 	require_once "Facebook/autoload.php";
 
 	$FB = new \Facebook\Facebook([
-		'app_id' => '179508382883634',
-		'app_secret' => 'c73ddaa44eba2e2c4845816d4fa32037',
+		'app_id' => '',
+		'app_secret' => '',
 		'default_graph_version' => 'v3.0'
 	]);
 
